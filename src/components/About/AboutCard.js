2d92862fcf92203591e666cb7681d1ed4bc8442d
HARIS AHMED <136917000+harisahmedjs@users.noreply.github.com>
2024-05-12 00:15:11 +0500

    not completed

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Haris Ahmed</span>&nbsp;
+            from <span className="purple">Karachi, Pakistan.</span>
+            <br />
+            I am currently looking for an nternship to level-up my skills.
+            <br />
+            I have completed my intermediate degree from Islamia govt. science college.
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Sports
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Workout
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
